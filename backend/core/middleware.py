260f5905489f8f013ade1bf4165d116a014a49d7
Raktim Yoddha <raktimyoddha07@gmail.com>
2026-06-01 19:21:24 +0530

    fixed hard coded forntend url to .env

--- backend/core/middleware.py
+++ backend/core/middleware.py
@@ -1,4 +1,12 @@
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+# Load .env variables
+load_dotenv()
+
+# Get FRONTEND_URL from .env
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 
 def setup_middleware(app):
@@ -6,7 +14,7 @@ def setup_middleware(app):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            "http://localhost:5173",
+            FRONTEND_URL,
         ],
         allow_credentials=True,
         allow_methods=["*"],
